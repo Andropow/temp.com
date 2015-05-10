5939552eb8cf2df69799a47207d8dc885a57438f
Andropow <kruzerandriy@gmail.com>
2015-05-10 23:12:56 +0300

    add login view

---
+++ views/vlogin.php
@@ -0,0 +1,13 @@
+<div class="hide">
+    <div id='fg_membersite'>
+        <div id="Sign-In">
+            <fieldset><legend>LOG-IN HERE</legend>
+                <form  method="POST" action="index.php">
+                    <label>User</label> <br><input type="text" name="user" size="40"><br>
+                    <label>Password</label> <br><input type="password" name="pass" size="40"><br>
+                    <button class="button" type="submit" name="submit" >Log-In</button>
+                </form>
+            </fieldset>
+        </div>
+    </div>
+</div>
