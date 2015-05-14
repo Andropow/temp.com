7f3a51099a4afc15cc84b3dbe136fcdb5dfcfcd3
Andropow <kruzerandriy@gmail.com>
2015-05-15 02:00:01 +0300

    add function change href for deleting page in loginscript

--- js/loginscript.js
+++ js/loginscript.js
@@ -6,20 +6,28 @@ $(document).ready(function () {
 //    $(".hide").click(function () {
 //        
 //    });//css("visibility", "hidden");
- $("textarea").ready(function(){
-     tinymce.init({
-        selector: "textarea",
-        language: 'uk_UA',
-        plugins: [
-            "advlist autolink link lists charmap preview hr anchor pagebreak spellchecker",
-            "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime nonbreaking",
-            "save table contextmenu directionality template paste textcolor",
-            "paste"
-        ],
-        content_css: "css/content.css",
-        paste_webkit_styles: "color font-size"
+    
+    $(".del").click(function (sender) {
+        if (confirm("Ви дійсьно хочете видалити сторінку?")) {
+            var curent = sender.currentTarget;
+            var path = curent.origin + curent.pathname + '?delete=' + sender.target.alt;
+            sender.currentTarget.href = path;
+        }
     });
- });
+
+    $("textarea").ready(function () {
+        tinymce.init({
+            selector: "textarea",
+            language: 'uk_UA',
+            plugins: [
+                "advlist autolink link lists charmap preview hr anchor pagebreak spellchecker",
+                "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime nonbreaking",
+                "save table contextmenu directionality template paste textcolor"
+            ],
+            content_css: "css/content.css",
+            paste_webkit_styles: "color font-size"
+        });
+    });
 });
 
 
